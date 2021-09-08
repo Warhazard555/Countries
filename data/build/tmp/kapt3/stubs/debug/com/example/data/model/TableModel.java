02e32daa03e7f259f4da745afda885ae191e28ad
Valentin <gyronk2@yandex.ru>
2021-09-08 18:55:40 +0300

    Filter Fix

--- data/build/tmp/kapt3/stubs/debug/com/example/data/model/TableModel.java
+++ data/build/tmp/kapt3/stubs/debug/com/example/data/model/TableModel.java
@@ -3,7 +3,7 @@ package com.example.data.model;
 import java.lang.System;
 
 @androidx.room.Entity(tableName = "Country")
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\u0007\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0012\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0087\b\u0018\u00002\u00020\u0001B5\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u0012\u0006\u0010\u0007\u001a\u00020\u0003\u0012\u0006\u0010\b\u001a\u00020\t\u0012\u0006\u0010\n\u001a\u00020\t\u00a2\u0006\u0002\u0010\u000bJ\t\u0010\u0015\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0016\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0017\u001a\u00020\u0006H\u00c6\u0003J\t\u0010\u0018\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0019\u001a\u00020\tH\u00c6\u0003J\t\u0010\u001a\u001a\u00020\tH\u00c6\u0003J\u0006\u0010\u001b\u001a\u00020\u001cJE\u0010\u001d\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00062\b\b\u0002\u0010\u0007\u001a\u00020\u00032\b\b\u0002\u0010\b\u001a\u00020\t2\b\b\u0002\u0010\n\u001a\u00020\tH\u00c6\u0001J\u0013\u0010\u001e\u001a\u00020\u001f2\b\u0010 \u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010!\u001a\u00020\tH\u00d6\u0001J\t\u0010\"\u001a\u00020\u0003H\u00d6\u0001R\u0016\u0010\u0005\u001a\u00020\u00068\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\rR\u0016\u0010\u0004\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\u000fR\u0016\u0010\n\u001a\u00020\t8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u0011R\u0016\u0010\u0007\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u000fR\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u000fR\u0016\u0010\b\u001a\u00020\t8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0011\u00a8\u0006#"}, d2 = {"Lcom/example/data/model/TableModel;", "", "name", "", "capital", "area", "", "language", "population", "", "currentDistance", "(Ljava/lang/String;Ljava/lang/String;FLjava/lang/String;II)V", "getArea", "()F", "getCapital", "()Ljava/lang/String;", "getCurrentDistance", "()I", "getLanguage", "getName", "getPopulation", "component1", "component2", "component3", "component4", "component5", "component6", "convertTableModelToDto", "Lcom/example/domain/dto/CountryItemDto;", "copy", "equals", "", "other", "hashCode", "toString", "data_debug"})
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\u0007\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0012\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0087\b\u0018\u00002\u00020\u0001B5\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u0012\u0006\u0010\u0007\u001a\u00020\u0003\u0012\u0006\u0010\b\u001a\u00020\t\u0012\u0006\u0010\n\u001a\u00020\t\u00a2\u0006\u0002\u0010\u000bJ\t\u0010\u0015\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0016\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0017\u001a\u00020\u0006H\u00c6\u0003J\t\u0010\u0018\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0019\u001a\u00020\tH\u00c6\u0003J\t\u0010\u001a\u001a\u00020\tH\u00c6\u0003J\u0006\u0010\u001b\u001a\u00020\u001cJE\u0010\u001d\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00062\b\b\u0002\u0010\u0007\u001a\u00020\u00032\b\b\u0002\u0010\b\u001a\u00020\t2\b\b\u0002\u0010\n\u001a\u00020\tH\u00c6\u0001J\u0013\u0010\u001e\u001a\u00020\u001f2\b\u0010 \u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010!\u001a\u00020\tH\u00d6\u0001J\t\u0010\"\u001a\u00020\u0003H\u00d6\u0001R\u0016\u0010\u0005\u001a\u00020\u00068\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\rR\u0016\u0010\u0004\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\u000fR\u0016\u0010\n\u001a\u00020\t8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u0011R\u0016\u0010\u0007\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u000fR\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u000fR\u0016\u0010\b\u001a\u00020\t8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0011\u00a8\u0006#"}, d2 = {"Lcom/example/data/model/TableModel;", "", "name", "", "capital", "area", "", "flag", "population", "", "currentDistance", "(Ljava/lang/String;Ljava/lang/String;FLjava/lang/String;II)V", "getArea", "()F", "getCapital", "()Ljava/lang/String;", "getCurrentDistance", "()I", "getFlag", "getName", "getPopulation", "component1", "component2", "component3", "component4", "component5", "component6", "convertTableModelToDto", "Lcom/example/domain/dto/CountryItemDto;", "copy", "equals", "", "other", "hashCode", "toString", "data_debug"})
 public final class TableModel {
     @org.jetbrains.annotations.NotNull
     @androidx.room.ColumnInfo
@@ -16,7 +16,7 @@ public final class TableModel {
     private final float area = 0.0F;
     @org.jetbrains.annotations.NotNull
     @androidx.room.ColumnInfo
-    private final java.lang.String language = null;
+    private final java.lang.String flag = null;
     @androidx.room.ColumnInfo
     private final int population = 0;
     @androidx.room.ColumnInfo
@@ -26,7 +26,7 @@ public final class TableModel {
     public final com.example.data.model.TableModel copy(@org.jetbrains.annotations.NotNull
     java.lang.String name, @org.jetbrains.annotations.NotNull
     java.lang.String capital, float area, @org.jetbrains.annotations.NotNull
-    java.lang.String language, int population, int currentDistance) {
+    java.lang.String flag, int population, int currentDistance) {
         return null;
     }
     
@@ -50,7 +50,7 @@ public final class TableModel {
     public TableModel(@org.jetbrains.annotations.NotNull
     java.lang.String name, @org.jetbrains.annotations.NotNull
     java.lang.String capital, float area, @org.jetbrains.annotations.NotNull
-    java.lang.String language, int population, int currentDistance) {
+    java.lang.String flag, int population, int currentDistance) {
         super();
     }
     
@@ -88,7 +88,7 @@ public final class TableModel {
     }
     
     @org.jetbrains.annotations.NotNull
-    public final java.lang.String getLanguage() {
+    public final java.lang.String getFlag() {
         return null;
     }
     
